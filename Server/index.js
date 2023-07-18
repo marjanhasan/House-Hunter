61e30e4e6f9d0b3c099a4250c712c13e724f3e17
marjanhasan <moonmarjanhasan@gmail.com>
2023-07-19 03:15:00 +0600

    some basic crud operations added using mongodb

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const connectDb = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://House-Hunter:SBl31RgWPcHKSMca@cluster0.vucpekr.mongodb.net/?retryWrites=true&w=majority"
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vucpekr.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("db connected");
   } catch (error) {
@@ -17,6 +17,84 @@ const connectDb = async () => {
     process.exit(1);
   }
 };
+
+// schema
+const houseSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  city: String,
+  bedrooms: Number,
+  bathrooms: Number,
+  size: String,
+  picture: String,
+  available: String,
+  rent: Number,
+  phone: Number,
+  description: String,
+});
+
+// modal
+const House = mongoose.model("Houses", houseSchema);
+
+app.post("/addhouse", async (req, res) => {
+  try {
+    const newHouse = new House({
+      name: req.body.name,
+      address: req.body.address,
+      city: req.body.city,
+      bedrooms: req.body.bedrooms,
+      bathrooms: req.body.bathrooms,
+      size: req.body.size,
+      picture: req.body.picture,
+      available: req.body.available,
+      rent: req.body.rent,
+      phone: req.body.phone,
+      description: req.body.description,
+    });
+    const houseData = await newHouse.save();
+    res.status(201).send(houseData);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/houses", async (req, res) => {
+  try {
+    const houses = await House.find();
+    if (houses) {
+      res.status(200).send({
+        success: true,
+        message: "Returned houses",
+        data: houses,
+      });
+    } else {
+      res.status(404).send({
+        success: false,
+        message: "Houses not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.delete("/houses/:id", async (req, res) => {
+  try {
+    const house = await House.findOne({ _id: req.params.id });
+    if (house) {
+      await House.deleteOne({ _id: req.params.id });
+      res.status(200).send({
+        message: "House data is deleted",
+      });
+    } else {
+      res.status(404).send({ message: "House is not found with this id" });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
